43a36c61c60a80796ab306229f18b6baf7cd928e
Anbarasan G <anbarasan.gangadaran@velocix.com>
2019-12-12 16:29:08 +0530

    added logging to routes

--- secretsanta/__init__.py
+++ secretsanta/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -15,4 +19,17 @@ login = LoginManager(app)
 
 login.login_view = 'login'
 
+log_dir = app.config['LOG_DIR']
+if not os.path.exists(log_dir):
+    os.mkdir(log_dir)
+
+file_handler = RotatingFileHandler(log_dir + '/secretsanta.log', maxBytes=10240, backupCount=20)
+file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.DEBUG)
+
 from secretsanta import routes, models
+
+app.logger.info('secret santa startup complete')
